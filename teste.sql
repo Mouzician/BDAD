b08ad93730830fcbbb4c315d161ec636a6167621
Psouka <p4sousa@gmail.com>
2014-04-05 11:43:33 +0100

    teste+

--- teste.sql
+++ teste.sql
@@ -6,7 +6,19 @@ modelo NVARCHAR2(20) NOT NULL,
 combustivel NVARCHAR2(20) NOT NULL,
 velocidade INTEGER PRIMARY KEY,
 peso INTEGER PRIMARY KEY,
-cargaMAX INTEGER PRIMARY KEY
+cargaMAX INTEGER PRIMARY KEY,
+idLinhas INTEGER PRIMARY KEY REFERENCES Linhas(idLinhas)
+);
+
+CREATE TABLE ComboioPassageiros (
+idComboioP INTEGER PRIMARY KEY AUTOINCREMENT,
+modelo NVARCHAR2(20) NOT NULL,
+combustivel NVARCHAR2(20) NOT NULL,
+velocidade INTEGER PRIMARY KEY,
+peso INTEGER PRIMARY KEY,
+lotacao INTEGER PRIMARY KEY,
+idTipoDeServico NUMBER REFERENCES TipoDeServico(idTipoDeServico),
+idLinhas INTEGER PRIMARY KEY REFERENCES Linhas(idLinhas)
 );
 
 
@@ -16,20 +28,28 @@ CREATE TABLE TipoDeServico (
 );
 
 
-
-CREATE TABLE ComboioPassageiros (
-idComboioP INTEGER PRIMARY KEY AUTOINCREMENT,
-modelo NVARCHAR2(20) NOT NULL,
-combustivel NVARCHAR2(20) NOT NULL,
-velocidade INTEGER PRIMARY KEY,
-peso INTEGER PRIMARY KEY,
-lotacao INTEGER PRIMARY KEY,
-idTipoDeServico NUMBER REFERENCES TipoDeServico(idTipoDeServico)
+CREATE TABLE Linhas(
+	idLinhas INTEGER PRIMARY KEY AUTOINCREMENT,
+	duracao NUMBER CHECK (duracao > 0),
+	distancia NUMBER CHECK (duracao > 0)
 );
 
 
-CREAT TABLE Linhas(
-	idLinhas INTEGER PRIMARY KEY AUTOINCREMENT,
-	duracao NUMBER CHECK (duracao > 0),
-	duracao NUMBER CHECK (duracao > 0)
+CREATE TABLE Carruagem(
+	idCarruagem NUMBER PRIMARY KEY,
+	idComboioP INTEGER PRIMARY KEY REFERENCES ComboioPassageiros(idComboioP),
+    capacidade NUMBER CHECK (duracao > 0),
+	tipoProdutos NVARCHAR2(20) NOT NULL,,
+	custo NUMBER CHECK (duracao > 0),
+
+);
+
+
+CREATE TABLE Empresa(
+	idEmpresa NUMBER PRIMARY KEY,
+	idCarruagem NUMBER PRIMARY KEY REFERENCES Carruagem(idCarruagem),
+	idLinhas NTEGER PRIMARY KEY REFERENCES Linhas(idLinhas),
+    capacidade NUMBER CHECK (duracao > 0),
+	tipoProdutos NVARCHAR2(20) NOT NULL,,
+	custo NUMBER CHECK (duracao > 0),
 );
