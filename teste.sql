81a7bab90b76042edd4cbd80b3f6fd3725043cba
Psouka <p4sousa@gmail.com>
2014-04-05 11:51:23 +0100

    teste+

--- teste.sql
+++ teste.sql
@@ -24,32 +24,43 @@ idLinhas INTEGER PRIMARY KEY REFERENCES Linhas(idLinhas)
 
 CREATE TABLE TipoDeServico (
 	idTipoDeServico NUMBER PRIMARY KEY,
-	frequencia NVARCHAR2(20) NOT NULL,
+	frequencia NVARCHAR2(20) NOT NULL
 );
 
 
 CREATE TABLE Linhas(
 	idLinhas INTEGER PRIMARY KEY AUTOINCREMENT,
 	duracao NUMBER CHECK (duracao > 0),
-	distancia NUMBER CHECK (duracao > 0)
+	distancia NUMBER CHECK (distancia > 0)
 );
 
 
 CREATE TABLE Carruagem(
 	idCarruagem NUMBER PRIMARY KEY,
 	idComboioP INTEGER PRIMARY KEY REFERENCES ComboioPassageiros(idComboioP),
-    capacidade NUMBER CHECK (duracao > 0),
-	tipoProdutos NVARCHAR2(20) NOT NULL,,
-	custo NUMBER CHECK (duracao > 0),
-
+    capacidade NUMBER CHECK (capacidade > 0),
+	tipoProdutos NVARCHAR2(20) NOT NULL,
+	custo NUMBER CHECK (custo > 0)
 );
 
 
 CREATE TABLE Empresa(
 	idEmpresa NUMBER PRIMARY KEY,
 	idCarruagem NUMBER PRIMARY KEY REFERENCES Carruagem(idCarruagem),
-	idLinhas NTEGER PRIMARY KEY REFERENCES Linhas(idLinhas),
-    capacidade NUMBER CHECK (duracao > 0),
-	tipoProdutos NVARCHAR2(20) NOT NULL,,
-	custo NUMBER CHECK (duracao > 0),
+	tipoMercadorias NVARCHAR2(20) NOT NULL,
+	nome NVARCHAR2(20) NOT NULL
+);
+
+
+CREATE TABLE Estacao(
+	idEstacao NUMBER PRIMARY KEY AUTOINCREMENT,
+	idTipoEstacao NUMBER PRIMARY KEY REFERENCES TipoEstacao(idTipoEstacao),
+	idLinhas INTEGER PRIMARY KEY REFERENCES Linhas(idLinhas),
+	localidade NVARCHAR2(20) NOT NULL,
+);
+
+CREATE TABLE TipoEstacao(
+	idTipoEstacao NUMBER PRIMARY KEY,
+	nome NVARCHAR2(20) NOT NULL
+	-- recursos?
 );
