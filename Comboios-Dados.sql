f076e3a0c6e334a7466db255c79b968983694e7f
RuiDGoncalves <ruidcscg@sapo.pt>
2014-04-06 16:44:32 +0100

    Update Comboios-Dados.sql

--- Comboios-Dados.sql
+++ Comboios-Dados.sql
@@ -12,12 +12,12 @@ INSERT INTO TipoDeServico (idTipoDeServico, nome, frequencia)
 
 -- Inserts na tabela Linha
 
+INSERT INTO Linha (duracao, distancia) 
+        VALUES (50, 40);
 INSERT INTO Linha (idLinha, duracao, distancia) 
-        VALUES (1, 50, 40);
-INSERT INTO Linha (idLinha, duracao, distancia) 
-        VALUES (2, 45, 34);
+        VALUES (45, 34);
 INSERT INTO Linha (idLinha, duracao, distancia)
-        VALUES (3, 42, 30);
+        VALUES (42, 30);
 
 -- Inserts na tabela Comboio
 /*
@@ -125,14 +125,59 @@ INSERT INTO Pessoas (idPessoa, nome, morada, idade)
 INSERT INTO Pessoas (idPessoa, nome, morada, idade)
         VALUES (3, 'Julio Paulo da Sousa Ferreira', 'Rua D. Manuel III, 45', 54);
 */
-
 -- Inserts na tabela Classe
 
 INSERT INTO Classe (idClasse, nome)
-        VALUES (1, 'Sub-23');
+        VALUES (1, 'Idoso');
 INSERT INTO Classe (idClasse, nome)
-        VALUES (2, 'Crianca');
+        VALUES (2, 'Crianca até 4 anos, inclusive');
 INSERT INTO Classe (idClasse, nome)
-        VALUES (3, 'Idoso');
+        VALUES (3, 'Jovem');        
+        
+-- Inserts na tabela Clientes
+
+INSERT INTO Clientes (idClientes, nome, morada, idade, tipoContrato, profissao)
+        VALUES (1, 'Carolina Filipa da Silva Torres e Corte-Real', 'Avenida de Cima, 123', 21, 'Estudante Sub-23', 'Estudante');
+INSERT INTO Clientes (idClientes, nome, morada, idade, tipoContrato, profissao)
+        VALUES (2, 'Maria Gertrudes Gomes Teixeira', 'Avenida de Baixo, 321', 76, 'Nenhum', 'Reformado');
+INSERT INTO Clientes (idClientes, nome, morada, idade, tipoContrato, profissao)
+        VALUES (3, 'Julio Paulo da Sousa Ferreira', 'Rua D. Manuel III, 45', 3, 'Nenhum', 'Sem profissao');
+        
+-- Inserts na tabela ClasseClientes
+
+INSERT INTO ClasseClientes (idClasse, idClientes, desconto)
+        VALUES (2, 3, 100);
+INSERT INTO ClasseClientes (idClasse, idClientes, desconto)
+        VALUES (3, 1, 30);
+INSERT INTO ClasseClientes (idClasse, idClientes, desconto)
+        VALUES (1, 2, 50);
+
+-- Inserts na tabela Horario
+
+INSERT INTO Horario (idHorario)
+        VALUES (1);
+INSERT INTO Horario (idHorario)
+        VALUES (2);
+INSERT INTO Horario (idHorario)
+        VALUES (3);
+        
+-- Inserts na tabela Especialidade
+
+INSERT INTO Especialidade (idEspecialidade, nome, renumeracao)
+        VALUES (1, 'Maquinista', 1086);
+INSERT INTO Especialidade (idEspecialidade, nome, renumeracao)
+        VALUES (2, 'Operador de revisao e venda', 755);
+INSERT INTO Especialidade (idEspecialidade, nome, renumeracao)
+        VALUES (3, 'Assistente Comercial', 870);
+        
+-- Inserts na tabela Funcionarios
+
+INSERT INTO Funcionarios (nome, morada, idade, idFuncionarios, area, idEspecialidade)
+        VALUES ('José Mauro Sousa Miranda', 'Rua da Pilantragem, 123', 54, 1,);
+INSERT INTO Funcionarios (nome, morada, idade, idFuncionarios, area, idEspecialidade)
+        VALUES ();
+INSERT INTO Funcionarios (nome, morada, idade, idFuncionarios, area, idEspecialidade)
+        VALUES ();        
+        
         
 ----------------- STAND BY ----------------------
