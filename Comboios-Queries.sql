d5e1995a7889ce7934048c4dc0112f1dfbca5f9a
Mouzician <mfilipe_13@hotmail.com>
2014-05-29 15:40:32 +0100

    fs

--- Comboios-Queries.sql
+++ Comboios-Queries.sql
@@ -2,6 +2,11 @@ SELECT nome FROM Carruagem
 WHERE idCarruagem NOT IN
 (SELECT idCarruagem FROM Aluguer);
 
+SELECT nome COUNT(*) FROM Carruagem
+GROUP BY nrContrato;
+
+SELECT AVG(duracao), AVG(distancia), SUM(distancia), MAX(distancia), MAX(duracao), MIN(duracao), MIN(distancia) FROM Linhas;
+
 SELECT TipoDeServico.nome, ComboioPassageiros.idComboioPassageiros
 FROM TipoDeServico, ComboioPassageiros
 WHERE TipoDeServico.idTipoDeServico = ComboioPassageiros.idTipoDeServico; 
